91d55ac49ddaf13651c7e48ed10dc76ce556d06c
DESKTOP-74PVRBE\Hades <adrianstachowiak23@gmail.com>
2021-04-26 14:54:21 +0200

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(
@@ -9,8 +12,42 @@ void main() {
   );
 }
 
-Future<Response> fetchAlbum() {
-  return get(Uri.https('api.sampleapis.com', 'futurama/info'));
+class Info {
+  final int id;
+  final List<Creator> creators;
+
+  Info(this.id, this.creators);
+
+  factory Info.fromJson(Map<String, dynamic> json) {
+    final creators = <Creator>[];
+    final jsonCreators = json['creators'] as List;
+    for (int i = 0; i < jsonCreators.length; i++) {
+      final creator = Creator.fromJson(jsonCreators[i]);
+      creators.add(creator);
+    }
+    return Info(
+      json['id'],
+      creators,
+    );
+  }
+}
+
+class Creator {
+  final String name;
+
+  Creator(this.name);
+
+  factory Creator.fromJson(Map<String, dynamic> json) {
+    return Creator(
+      json['name'],
+    );
+  }
+}
+
+Future<Info> fetchAlbum() async {
+  final response = await get(Uri.https('api.sampleapis.com', 'futurama/info'));
+  final list = jsonDecode(response.body) as List;
+  return Info.fromJson(list[0]);
 }
 
 class PageOne extends StatelessWidget {
@@ -18,11 +55,26 @@ class PageOne extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FutureBuilder<Response>(
+        child: FutureBuilder<Info>(
           future: fetchAlbum(),
-          builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
+          builder: (BuildContext context, AsyncSnapshot<Info> snapshot) {
             if (snapshot.hasData) {
-              return Text(snapshot.data.toString());
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data.creators.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Text(snapshot.data.creators[index].name),
+                      FlatButton(
+                          onPressed: () {
+                            launch("https://www.google.pl");
+                          },
+                          child: Text("Click me"))
+                    ],
+                  );
+                },
+              );
             }
             return CircularProgressIndicator();
           },
