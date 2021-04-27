0ce5fa25b1fbf9bdd01f0a036eb1d38e24373d68
DESKTOP-74PVRBE\Hades <adrianstachowiak23@gmail.com>
2021-04-27 12:14:01 +0200

    Second commit 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,10 @@ void main() {
 class Info {
   final int id;
   final List<Creator> creators;
+  final String synopsis;
+  final String yearsAired;
 
-  Info(this.id, this.creators);
+  Info(this.id, this.creators, this.synopsis, this.yearsAired);
 
   factory Info.fromJson(Map<String, dynamic> json) {
     final creators = <Creator>[];
@@ -25,21 +27,26 @@ class Info {
       final creator = Creator.fromJson(jsonCreators[i]);
       creators.add(creator);
     }
+
     return Info(
       json['id'],
       creators,
+      json['synopsis'],
+      json['yearsAired'],
     );
   }
 }
 
 class Creator {
   final String name;
+  final String url;
 
-  Creator(this.name);
+  Creator(this.name, this.url);
 
   factory Creator.fromJson(Map<String, dynamic> json) {
     return Creator(
       json['name'],
+      json['url'],
     );
   }
 }
@@ -59,21 +66,35 @@ class PageOne extends StatelessWidget {
           future: fetchAlbum(),
           builder: (BuildContext context, AsyncSnapshot<Info> snapshot) {
             if (snapshot.hasData) {
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data.creators.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      Text(snapshot.data.creators[index].name),
-                      FlatButton(
-                          onPressed: () {
-                            launch("https://www.google.pl");
-                          },
-                          child: Text("Click me"))
-                    ],
-                  );
-                },
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    snapshot.data.synopsis,
+                    style: TextStyle(
+                      fontFamily: "Roboto",
+                    ),
+                  ),
+                  Text(snapshot.data.yearsAired),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data.creators.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Text(snapshot.data.creators[index].name),
+                          FlatButton(
+                              onPressed: () {
+                                launch(snapshot.data.creators[index].url);
+                              },
+                              child: Text("Click me")),
+                        ],
+                      );
+                    },
+                  ),
+                  Text(snapshot.data.id.toString()),
+                ],
               );
             }
             return CircularProgressIndicator();
